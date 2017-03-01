906b8f8d8b707243e3223e3a578180e9d41549b2
Chris <Chrisjhronek@gmail.com>
2017-02-28 20:06:46 -0600

    signup working all commands in functions

--- js/javascript.js
+++ js/javascript.js
@@ -1,127 +1,139 @@
+$(document).ready(function(){
 //pulls all of the healing posts in decending order
 //starting from the most recent post
-function queryFromDatabase(){
-	Parse.initialize("518e0dbca14e73748f81e550e12deea515ff959e");
-	Parse.serverURL = 'http://ec2-35-165-199-91.us-west-2.compute.amazonaws.com:80/parse';
-  var healings = Parse.Object.extend("Healing");
-	var query = new Parse.Query(healings);
-  //using healings object
+  function queryFromDatabase(){
+  	Parse.initialize("518e0dbca14e73748f81e550e12deea515ff959e");
+  	Parse.serverURL = 'http://ec2-35-165-199-91.us-west-2.compute.amazonaws.com:80/parse';
+    var healings = Parse.Object.extend("Healing");
+  	var query = new Parse.Query(healings);
+    //using healings object
 
- query.descending("updatedAt");
-  query.find({
-  success: function(results) {
-    //alert("Successfully retrieved " + results.length + " foos.");
+   query.descending("updatedAt");
+    query.find({
+    success: function(results) {
+      //alert("Successfully retrieved " + results.length + " foos.");
 
-    //print out each object in its own paragraph
-    for(var i = 0; i < results.length; i++){
-      var object = results[i];
-      document.getElementById("pullData").innerHTML += "<h4>" + object.get("title") + "</h4><p>" + object.get("body") + "</p>";
+      //print out each object in its own paragraph
+      for(var i = 0; i < results.length; i++){
+        var object = results[i];
+        document.getElementById("pullData").innerHTML += "<h4>" + object.get("title") + "</h4><p>" + object.get("body") + "</p>";
+      }
+    },
+  error: function(error) {
+      alert("Error: " + error.code + " " + error.message);
     }
-  },
-error: function(error) {
-    alert("Error: " + error.code + " " + error.message);
+  });
+  };
+
+  //creates a new account requiring:
+  //email, username, password, first and last name
+  //has a check to make assure you have entered the same
+  //password twice
+  function signup(){
+  $("#signup").click(function(event){
+    Parse.initialize("518e0dbca14e73748f81e550e12deea515ff959e");
+    Parse.serverURL = "http://ec2-35-165-199-91.us-west-2.compute.amazonaws.com:80/parse";
+    event.preventDefault();
+
+    //build a check for password and require all values
+    var username = $("#createUsername").val();
+    var password = $("#createUserPass").val();
+    var firstname = $("#createFirstname").val();
+    var lastname = $("#createLastname").val();
+    //var email = $("#createEmail").val();
+
+    //create new user
+    var user = new Parse.User();
+
+    //set values for user
+    user.set("username", username);
+    user.set("password", password);
+    user.set("firstName", firstname);
+    user.set("lastName", lastname);
+    //user.set("email", email);
+
+
+      //create the account
+      user.signUp( null, {
+          success:function(user){
+
+          }, error: function(user, error){
+           console.error("signup error:", error)
+          }
+      });
+  });
   }
-});
-}
 
-//creates a new account requiring:
-//email, username, password, first and last name
 
-//has a check to make assure you have entered the same
-//password twice
-function signUp(){
-$("#signup").submit(function(event){
-Parse.initialize("518e0dbca14e73748f81e550e12deea515ff959e");
-Parse.serverURL = "http://ec2-35-165-199-91.us-west-2.compute.amazonaws.com:80/parse";
-event.preventDefault();
+  //posts a healing, with anonymous capabilities
+  function postHealing(){
+  $("#postHealing").submit(function(event){
+    Parse.initialize("518e0dbca14e73748f81e550e12deea515ff959e");
+    Parse.serverURL = 'http://ec2-35-165-199-91.us-west-2.compute.amazonaws.com:80/parse';
+    event.preventDefault();
 
-//build a check for password and require all values
-var username = $("#createUsername").val();
-var password = $("#createUserPass").val();
-var firstname = $("#createFirstname").val();
-var lastname = $("#createLastname").val();
-//var email = $("#createEmail").val();
+    //build a check for password and require all values
+    var title = $("#title").val();
+    var body = $("#body").val();
+    var anon = $("#anon").val();
+    //set user value with who is logged in currently
 
-//create new user
-var user = new Parse.User();
+    //make a new healing object
+    var Healing = Parse.Object.extend("Healing");
+    var newHealing = new Healing();
 
-//set values for user
-user.set("username", username);
-user.set("password", password);
-user.set("firstName", firstname);
-user.set("lastName", lastname);
-//user.set("email", email);
+    //store info in that object
+    newHealing.set("title", title);
+    newHealing.set("body", body);
+    newHealing.set("anon", anon);
+    //newHealing.set("", username);
 
-  //create the account
-  user.signUp( null, {
-      success:function(user){
+    //save the healng contents
+    newHealing.save(null, {
+      success: function(Healing) {
+        alert('New healing created with title: ' + Healing.get("title"));
+    
+      }, error: function(Healing, error) {
+        console.error('Failed to create new healing, with error code: ' + error.message);
+      }
+    });
+  });
+  }
 
-      }, error: function(user, error){
-       console.error("signup error:", error)
+
+  //grabs variables in login form logs in
+  //the user to their correct account
+  function login(){
+  $("#login").click(function(event){
+    Parse.initialize("518e0dbca14e73748f81e550e12deea515ff959e");
+    Parse.serverURL = 'http://ec2-35-165-199-91.us-west-2.compute.amazonaws.com:80/parse';
+    event.preventDefault();
+
+    var loginUser = $("#loginUsername").val();
+    var loginPass = $("#loginPassword").val();
+
+    alert(loginUser + " " + loginPass);
+
+    Parse.User.logIn(loginUser, loginPass, {
+      success: function(user) {
+        console.log(user);
+        //send user to their feed page
+        // Do stuff after successful login.
+      },
+      error: function(user, error) {
+        console.log(user + error);
+        // The login failed. Check error to see why.
       }
   });
-});
-}
-
-//posts a healing, with anonymous capabilities
-function postHealing(){
-$("#postHealing").submit(function(event){
-  Parse.initialize("518e0dbca14e73748f81e550e12deea515ff959e");
-  Parse.serverURL = 'http://ec2-35-165-199-91.us-west-2.compute.amazonaws.com:80/parse';
-  event.preventDefault();
-
-  //build a check for password and require all values
-  var title = $("#title").val();
-  var body = $("#body").val();
-  var anon = $("#anon").val();
-  //set user value with who is logged in currently
-
-  //make a new healing object
-  var Healing = Parse.Object.extend("Healing");
-  var newHealing = new Healing();
-
-  //store info in that object
-  newHealing.set("title", title);
-  newHealing.set("body", body);
-  //newHealing.set("anon", anon);
-  //newHealing.set("", username);
-
-  //save the healng contents
-  newHealing.save(null, {
-    success: function(Healing) {
-      alert('New healing created with title: ' + Healing.get("title"));
-  
-    }, error: function(Healing, error) {
-      console.error('Failed to create new healing, with error code: ' + error.message);
-    }
   });
+  }
+
+  // function logout(){
+  //    http://stackoverflow.com/questions/29952249/how-to-create-log-out-script-parse-javascript
+  //   $("$logout").click(function(event)){
+  //     if(Parse.User.current())
+  //     Parse.User.logout();
+  //   }
+  // }
+
 });
-}
-
-
-//grabs variables in login form logs in
-//the user to their correct account
-function login(){
-$("#login").submit(function(event){
-  Parse.initialize("518e0dbca14e73748f81e550e12deea515ff959e");
-  Parse.serverURL = 'http://ec2-35-165-199-91.us-west-2.compute.amazonaws.com:80/parse';
-  event.preventDefault();
-
-  var loginUser = $("#loginUsername").val();
-  var loginPass = $("#loginPassword").val();
-
-  alert(loginUser + " " + loginPass);
-
-  Parse.User.logIn(loginUser, loginPass, {
-    success: function(user) {
-      console.log(user);
-      //send user to their feed page
-      // Do stuff after successful login.
-    },
-    error: function(user, error) {
-      console.log(user + error);
-      // The login failed. Check error to see why.
-    }
-});
-});
-}
